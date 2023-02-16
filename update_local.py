ef8be0110128b55943be01883cecc9401b90a6c2
Chris Musson <chris.musson@hotmail.com>
2023-02-16 15:35:09 +0000

    Update ignored matches

--- update_local.py
+++ update_local.py
@@ -6,40 +6,42 @@ import time
 
 
 def update_local(competition, season):
-
     # set of matches to ignore for various reasons
-    # currently, all matches in here are those that are in the bundesliga/ligue1 relegation playoff
+    # currently, most matches in here are those that are in the bundesliga/ligue1 relegation playoff phases
     # I have decided to ignore these as they are not part of the regular season, and
     # FBRef has no player data for these matches anyway
-    bundesliga_ignore = set(
-        [
-            "5dc40876",
-            "9c6a24db",
-            "e262266b",
-            "f5e7a5c2",
-            "434865ef",
-            "2f2a35fa",
-            "dc47142c",
-            "948872ab",
-            "ac3eb7f6",
-            "d50b48fe",
-        ]
-    )
+    # Other matches that have been abandoned are also included, such as Bochum vs. Monchengladbach,
+    # where the game was abandoned after an assistant referee was hit by something thrown from the stands
+    bundesliga_relegation = {
+        "5dc40876",
+        "9c6a24db",
+        "e262266b",
+        "f5e7a5c2",
+        "434865ef",
+        "2f2a35fa",
+        "dc47142c",
+        "948872ab",
+        "ac3eb7f6",
+        "d50b48fe",
+    }
 
-    ligue1_ignore = set(
-        [
-            "f927719d",
-            "06517ea5",
-            "bfd434c1",
-            "ea5db1c4",
-            "621f8a81",
-            "28ce9808",
-            "f1560d55",
-            "78ed8c0d",
-        ]
-    )
+    ligue1_relegation = {
+        "f927719d",
+        "06517ea5",
+        "bfd434c1",
+        "ea5db1c4",
+        "621f8a81",
+        "28ce9808",
+        "f1560d55",
+        "78ed8c0d",
+    }
 
-    ignored_matches = bundesliga_ignore | ligue1_ignore
+    other_ignore = {
+        "e0a20cfe",  # SerieA_2020-2021 - Verona:Roma - Result Awarded - Registration Error
+        "c34bbc21",  # Bundesliga_2021-2022 - Bochum:Monchengladbach - Abandoned - Fan Trouble
+    }
+
+    ignored_matches = bundesliga_relegation | ligue1_relegation | other_ignore
 
     stored_files = set(os.listdir(os.path.join("web_pages", competition, season)))
     with open("db_helper.json", "r") as f:
@@ -63,7 +65,6 @@ def update_local(competition, season):
 
     if n == 0:
         print(f"Local files are up to date for {competition} {season}")
-        time.sleep(5)
         return
 
     print(f"Fetching {n} {'matches' if n > 1 else 'match'}")
